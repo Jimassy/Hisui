392f0c9b5e993bc0cd2d4e6e90a3d25d79938495
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2018-10-20 00:02:22 +0900

    add snake yaml and new class

---
+++ src/main/java/Method/Slot.java
@@ -0,0 +1,14 @@
+package Method;
+
+import net.dv8tion.jda.core.entities.User;
+
+public class Slot {
+	public void doMethod() {
+
+	}
+
+
+	public void save(User user, int data) {
+		//Yaml yaml = new Yaml();
+	}
+}
