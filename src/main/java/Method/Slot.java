689731db8503926398cc9afbc59b5916477102b1
佐竹涼太 <picoreamosaiko@docomo.ne.jp>
2018-10-20 09:19:56 +0900

    unable yaml

--- src/main/java/Method/Slot.java
+++ src/main/java/Method/Slot.java
@@ -1,14 +1,14 @@
 package Method;
 
 import net.dv8tion.jda.core.entities.User;
+import org.yaml.snakeyaml.*;
 
 public class Slot {
 	public void doMethod() {
 
 	}
 
-
 	public void save(User user, int data) {
-		//Yaml yaml = new Yaml();
+		Yaml yaml = new Yaml();
 	}
 }
